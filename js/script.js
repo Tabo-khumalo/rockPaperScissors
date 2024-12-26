f1f276f513cb2fccabe728e6668fc68452a7ab6d
Tabo-khumalo <tabo.khumalo26@gmail.com>
2024-12-26 20:40:43 +0200

    Add JS functionity
    Add functionality to button and displayed the results in the dom

--- js/script.js
+++ js/script.js
@@ -1,133 +1,171 @@
 
 // let humanSelection = getHumanChoice();
-// let computerSelection = getComputerchoice();
-// let humanScore = 0;
-// let computerScore = 0;
+let computerSelection = getComputerchoice();
+let humanScore = 0;
+let computerScore = 0;
 
-// function getComputerchoice() {
-//     let computerMove = Math.floor(Math.random() * 75);
-
-//     if (computerMove >= 0 && computerMove <= 25) {
-//         return "Rock";
-//     } 
-//     else if(computerMove > 25 && computerMove <= 50) {
-//         return "Paper";
-//     } 
-//      else {
-//         return "Scissors";
-//      }
-//     }
+// Human Selection
+const rockBtn = document.getElementById("rock").addEventListener("click", playRound)
+const paperBtn = document.getElementById("paper").addEventListener("click", playRound)
+const scissorsBtn = document.getElementById("scissors").addEventListener("click", playRound)
 
 
-// function getHumanChoice(){
-//     let userPrompt = prompt("Enter Rock, Paper or Scissors to Play", "");
-//     let userPromptFirstLetter = userPrompt.at(0).toLocaleUpperCase();
-//     let userPromptLowerCase = userPrompt.slice(1).toLocaleLowerCase()
+// score variable
 
-//     return userPromptFirstLetter + userPromptLowerCase;  
-// }
+let playerScore = document.getElementById("player-score");
+let rockPlayer = document.getElementById("rock-player-score");
+let paperPlayer = document.getElementById("paper-player-score");
+let scissorPlayer = document.getElementById("scissor-player-score");
+let score = 0;
 
+// Round Result
 
-// function playRound(humanChoice, computerChoice) {
+PlayerChoiceResult = document.getElementById("player-choice-result");
+computerChoiceResult = document.getElementById("computer-choice-result");
+roundResult = document.getElementById("round-result-content");
 
-//     let result = "";
+function getComputerchoice() {
+    let computerMove = Math.floor(Math.random() * 75);
 
-//     if(humanChoice === "Rock" && computerChoice === "Scissors") {
-//         humanScore++;
-//         result = console.log(`
+    if (computerMove >= 0 && computerMove <= 25) {
+        return "Rock";
+    } 
+    else if(computerMove > 25 && computerMove <= 50) {
+        return "Paper";
+    } 
+     else {
+        return "Scissors";
+     }
+    }
 
-//             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-            
-//         `)
-//     } 
-//     else if (humanChoice === "Scissors" && computerChoice === "Paper") { 
-//         humanScore++;
-//         result = console.log(`
+function playRound(e) {
 
-//             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-//         `)
-//     }
-//     else if (humanChoice === "Paper" && computerChoice === "Rock") { 
-//         humanScore++;
+    if (e.target.id === "rock-icon" && computerSelection === "Scissor") {
+        
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/Scissor Icon right.png" alt="rock paper scissors icon">
+        `
+
+        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection}
+            Yippy, You win this round!
+        `;
+    } 
+    else if (e.target.id === "rock-icon" && computerSelection === "Paper") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection} 
+            computer wins this round, Keep trying!
+        `;
+
+    } 
+    else if(e.target.id === "rock-icon" && computerSelection === "Rock") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection}
+        it's a tie, try again.
+        `;
+
+    } 
+    else if(e.target.id === "paper-icon" && computerSelection === "Rock") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}
+        You are a natural, You win.
+        `;
+    }
+    else if(e.target.id === "paper-icon" && computerSelection === "Scissor") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/Scissor Icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}
+        Computer Wins, You can do it.
+        `
+    }
+    else if(e.target.id === "paper-icon" && computerSelection === "Paper") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}.
+        You Tied, great minds think alike! go on.
+        `
+    } 
+    else if(e.target.id === "scissors-icon" && computerSelection === "Paper") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
+        `
+        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
+        You win, well done!
+        `
+    }
+    else if(e.target.id === "scissors-icon" && computerSelection === "Rock") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icons">
+        `
+        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
+        Computer wins, Keep going!
+        `
+    }
+    else if(e.target.id === "scissors-icon" && computerSelection === "scissors") {
+        PlayerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
+        `
+
+        computerChoiceResult.innerHTML = `
+            <img id="player-choice-icon" src="/images/Scissor Icon right.png" alt="rock paper scissors icons">
+        `
+        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
+        twins! carry on!
+        `
+    }
+}
+
+    console.log(e.target.id)
+    console.log(computerSelection)
+ 
+
     
-//         result = console.log(`
 
-//             You picked ${humanChoice} and Computer picked ${computerChoice} you win!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-//         `)
-//     }
-//     else if (humanChoice === "Scissors" && computerChoice === "Rock") { 
-//         ccomputerScore++;
-//         result = console.log(`
-
-//             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-//         `)
-//     }
-//     else if (humanChoice === "Rock" && computerChoice === "Paper") { 
-//         computerScore++;
-//         result = console.log(`
-
-//             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-//         `)
-//     }
-//     else if (humanChoice === "Paper" && computerChoice === "Scissor") { 
-//         computerScore++;
-//         result = console.log(`
-
-//             You picked ${humanChoice} and Computer picked ${computerChoice} Computer wins!!
-//             Scores: 
-//             Player Score: ${humanScore}
-//             Computer Score: ${computerScore}
-//         `)
-//     } else  if(humanChoice === computerChoice){
-//         result = console.log(`You picked ${humanChoice} and Computer picked ${computerChoice} it's a tie`)
-//     }
-
-//     return result
-// }
-
-
-
-
-// // function playGame() {
-// //     for(let i = 1; i < 6; i++) {
-// //         const humanSelection = getHumanChoice();
-// //         const computerSelection = getComputerchoice();
-// //         playRound(humanSelection, computerSelection);
-// //     }
-
-// //     if(computerScore > humanScore) {
-// //         return `
-// //         Computer Score: ${computerScore} - Human Score: ${humanScore}
-// //         the computer beat you. try again`
-// //     }
-// //     else if(computerScore < humanScore) {
-// //         return `
-// //         Computer Score: ${computerScore} - Human Score: ${humanScore}
-// //         You beat the computer, way to go!`
-// //     } else {
-// //         return `
-// //         Computer Score: ${computerScore} - Human Score: ${humanScore}
-// //         it's a tie, not bad!`
+function playGame() {
+    playRound(e)
     
-// // }
+}
 
-// console.log(playGame())
-
+playGame()
 
 
 
