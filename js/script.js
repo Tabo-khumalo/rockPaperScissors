102087d74bb158f7e2bef2ddf8a1738197eb87fa
Tabo-khumalo <tabo.khumalo26@gmail.com>
2025-01-06 21:18:01 +0200

    finalized code for the game
    rework the logic and added reset btn for the game.

--- js/script.js
+++ js/script.js
@@ -1,208 +1,259 @@
+// Score container
 
-// Computer Selection
-
-let computerSelection = getComputerchoice();
-
-// Human Selection
-
-const rockBtn = document.getElementById("rock").addEventListener("click", playRound)
-const paperBtn = document.getElementById("paper").addEventListener("click", playRound)
-const scissorsBtn = document.getElementById("scissors").addEventListener("click", playRound)
+// round
 
 
-// score variable
 
-let playerScore = document.getElementById("player-score");
-let rockPlayer = document.getElementById("rock-player-score");
-let paperPlayer = document.getElementById("paper-player-score");
-let scissorPlayer = document.getElementById("scissor-player-score");
+// player and computer scores counter
 
-let computerScore = document.getElementById("computer-score");
-let rockComputer = document.getElementById("rock-computer-score");
-let paperComputer = document.getElementById("paper-computer-score");
-let scissorComputer = document.getElementById("scissors-computer-score");
+// let playerScore = document.getElementById("player-score")
+// let computerScore = document.getElementById("computer-score")
 
+// player rock, paper, scissors count
 
-// Round Result
+// let rockPlayerScore = document.getElementById("rock-player-score")
+// let paperPlayerScore = document.getElementById("paper-player-score")
+// let scissorPlayerScore = document.getElementById("scissor-player-score")
 
-PlayerChoiceResult = document.getElementById("player-choice-result");
-computerChoiceResult = document.getElementById("computer-choice-result");
-roundResult = document.getElementById("round-result-content");
+// computer rock, paper, scissors count
 
-function getComputerchoice() {
-    let computerMove = Math.floor(Math.random() * 75);
+// let rockComputerScore = document.getElementById("rock-computer-score")
+// let paperComputerScore = document.getElementById("paper-computer-score")
+// let scissorComputerScore = document.getElementById("scissor-computer-score")
 
-    if (computerMove >= 0 && computerMove <= 25) {
+// -----------------------------------------------------------------------------------------
+
+// funtion to get computer's choice
+
+function getComputerChoice () {
+    let computerMove = Math.floor(Math.random() * 100)
+
+    if(computerMove >= 0 && computerMove <= 33) {
         return "Rock";
-    } 
-    else if(computerMove > 25 && computerMove <= 50) {
-        return "Paper";
-    } 
-     else if (computerMove > 50 && computerMove <= 75) {
+    }
+    else if(computerMove > 33 && computerMove <= 66 ) {
+        return  "Paper";
+    }
+    else {
         return "Scissors";
-
-     }
     }
 
-function playRound(e) {
+};
 
-    let computerSelection = getComputerchoice();
+// -------------------------------------------------------------------------------------------
 
-    if (e.target.id === "rock-icon" && computerSelection === "Scissors") {
-        
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
-        `
+// Event Listeners to get the player choice
 
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/Scissor Icon right.png" alt="rock paper scissors icon">
-        `
+const rockBtn = document.getElementById("rock").addEventListener("click", playRound);
+const paperBtn = document.getElementById("paper").addEventListener("click", playRound);
+const scissorsBtn = document.getElementById("scissors").addEventListener("click", playRound);
 
-        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection}.
-            Yippy, You win this round!
-        `;
+// ............................................................................................
 
-        playerScore.innerHTML++;
-        rockPlayer.innerHTML++;
-        scissorComputer.innerHTML++;
-    } 
-    else if (e.target.id === "rock-icon" && computerSelection === "Paper") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
-        `
+// choice result
 
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
-        `
-        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection}. 
+// Player choice result
+
+let playerChoiceResult = document.getElementById("player-choice-result");
+let playerResultIcon = document.createElement("img"); 
+playerResultIcon.id = "player-choice-icon"
+
+// computer choice result 
+
+let computerChoiceResult = document.getElementById("computer-choice-result");
+let computerResultIcon = document.createElement("img");
+computerResultIcon.id = "computer-choice-icon";
+
+// round results
+
+let roundResult = document.getElementById("round-results");
+
+
+
+    let round = document.getElementById("current-round");
+    let playerScore = document.getElementById("player-score");
+    let computerScore = document.getElementById("computer-score");
+    let rockPlayerScore = document.getElementById("rock-player-score");
+    let paperPlayerScore = document.getElementById("paper-player-score");
+    let scissorPlayerScore = document.getElementById("scissor-player-score");
+    let rockComputerScore = document.getElementById("rock-computer-score");
+    let paperComputerScore = document.getElementById("paper-computer-score");
+    let scissorComputerScore = document.getElementById("scissor-computer-score");
+    let rockChoiceBtn = document.getElementById("rock");
+    let resetBtn = document.getElementById("reset");
+    let choiceDiv = document.getElementById("choice");
+
+function playRound (e) {
+
+    let computerChoice = getComputerChoice();
+
+    if(e.target.id === "rock" && computerChoice === "Paper") {
+        playerResultIcon.setAttribute("src", "./images/rock icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/Paper Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon)
+        roundResult.textContent = `
+            You picked Rock and Computer picked ${computerChoice}. 
             Computer wins this round, Keep trying!
         `;
 
-        computerScore.innerHTML++;
-        rockPlayer.innerHTML++;
-        paperComputer.innerHTML++;
-
+        round.textContent++;
+        computerScore.textContent++
+        rockPlayerScore.textContent++;
+        paperComputerScore.textContent++;
     } 
-    else if(e.target.id === "rock-icon" && computerSelection === "Rock") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/rock icon.png" alt="rock paper scissors icons">
+    else if(e.target.id === "rock" && computerChoice === "Scissors") {
+        playerResultIcon.setAttribute("src", "./images/rock icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/Scissor Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon)
+        roundResult.textContent = `
+            You picked Rock and Computer picked ${computerChoice}.
+                Yippy, You win this round!
         `
+        round.textContent++;
+        playerScore.textContent++;
+        rockPlayerScore.textContent++;
+        scissorComputerScore.textContent++
+    }
+    else if(e.target.id === "rock" && computerChoice === "Rock") {
+        playerResultIcon.setAttribute("src", "./images/rock icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/rock icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon);
+        roundResult.textContent = `
+            You picked Scissors and Computer picked ${computerChoice}.
+            it's a tie, try again.
+        `
+        round.textContent++;
+        rockPlayerScore.textContent++;
+        rockComputerScore.textContent++
 
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icon">
+    }
+    else if(e.target.id === "paper" && computerChoice === "Scissors") {
+        playerResultIcon.setAttribute("src", "./images/Paper Icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/Scissor Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon)
+        roundResult.textContent = `
+            You picked Paper and Computer picked ${computerChoice}.
+            Computer Wins, You can do it!
         `
-        roundResult.textContent = `You picked Rock and Computer picked ${computerSelection}.
-        it's a tie, try again.
+        round.textContent++;
+        computerScore.textContent++;
+        paperPlayerScore.textContent++;
+        scissorComputerScore.textContent++;
+    }
+    else if(e.target.id === "paper" && computerChoice === "Rock") {
+        playerResultIcon.setAttribute("src", "./images/Paper Icon.png");
+        playerChoiceResult.appendChild(playerResultIcon);
+        computerResultIcon.setAttribute("src", "./images/rock icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon);
+        roundResult.textContent = `
+            You picked Paper and Computer picked ${computerChoice}.
+            You are a natural, You win!
         `;
+        round.textContent++;
+        playerScore.textContent++;
+        paperPlayerScore.textContent++;
+        rockComputerScore.textContent++;
+    }
+    else if(e.target.id === "paper" && computerChoice === "Paper") {
+        playerResultIcon.setAttribute("src", "./images/Paper Icon.png")
+        playerChoiceResult.appendChild(playerResultIcon);
+        computerResultIcon.setAttribute("src", "./images/Paper Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon);
+        roundResult.textContent = `
+            You picked Paper and Computer picked ${computerChoice}.
+            You Tied, great minds think alike! go on.
+        `
+        round.textContent++;
+        paperPlayerScore.textContent++;
+        paperComputerScore.textContent++
 
-        rockComputer.innerHTML++;
-        rockPlayer.innerHTML++;
-
+    }
+    else if(e.target.id === "scissors" && computerChoice === "Rock") {
+        playerResultIcon.setAttribute("src", "./images/Scissor Icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/rock icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon)
+        roundResult.textContent = `
+            You picked Scissors and Computer picked ${computerChoice}.
+            Computer wins, Keep going!
+        `
+        round.textContent++;
+        computerScore.textContent++;
+        scissorPlayerScore.textContent++;
+        rockComputerScore.textContent++;
     } 
-    else if(e.target.id === "paper-icon" && computerSelection === "Rock") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
+    else if(e.target.id === "scissors" && computerChoice === "Paper") {
+        playerResultIcon.setAttribute("src", "./images/Scissor Icon.png")
+        playerChoiceResult.appendChild(playerResultIcon)
+        computerResultIcon.setAttribute("src", "./images/Paper Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon)
+        roundResult.textContent = `
+            You picked Scissors and Computer picked ${computerChoice}.
+            Computer wins, Keep going!
         `
-
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icon">
-        `
-        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}.
-        You are a natural, You win.
-        `; 
-
-        playerScore.innerHTML++;
-        paperPlayer.innerHTML++;
-        rockComputer.innerHTML++;
+        round.textContent++;
+        playerScore.textContent++;
+        scissorPlayerScore.textContent++;
+        paperComputerScore.textContent++;
     }
-    else if(e.target.id === "paper-icon" && computerSelection === "Scissors") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
+    else if(e.target.id === "scissors" && computerChoice === "Scissors") {
+        playerResultIcon.setAttribute("src", "./images/Scissor Icon.png")
+        playerChoiceResult.appendChild(playerResultIcon);
+        computerResultIcon.setAttribute("src", "./images/Scissor Icon right.png");
+        computerChoiceResult.appendChild(computerResultIcon);
+        roundResult.textContent = `
+            You picked Scissors and Computer picked ${computerChoice}.
+            Twins! carry on!
         `
+        round.textContent++;
+        scissorPlayerScore.textContent++;
+        scissorComputerScore.textContent++
 
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/Scissor Icon right.png" alt="rock paper scissors icon">
-        `
-        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}.
-        Computer Wins, You can do it!
-        `
-        computerScore.innerHTML++;
-        paperPlayer.innerHTML++;
-        scissorComputer.innerHTML++;
-        
     }
-    else if(e.target.id === "paper-icon" && computerSelection === "Paper") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Paper Icon.png" alt="rock paper scissors icons">
-        `
-
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
-        `
-        roundResult.textContent = `You picked Paper and Computer picked ${computerSelection}.
-        You Tied, great minds think alike! go on.
-        `
-        paperPlayer.innerHTML++;
-        paperComputer.innerHTML++;
-    } 
-    else if(e.target.id === "scissors-icon" && computerSelection === "Paper") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
-        `
-
-        computerChoiceResult.innerHTML = `
-            <img id="computer-choice-icon" src="./images/Paper Icon right.png" alt="rock paper scissors icon">
-        `
-        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
-        You win, well done!
-        `
-        playerScore.innerHTML++;
-        scissorPlayer.innerHTML++;
-        paperComputer++;
-    }
-    else if(e.target.id === "scissors-icon" && computerSelection === "Rock") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
-        `
-
-        computerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/rock icon right.png" alt="rock paper scissors icons">
-        `
-        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
-        Computer wins, Keep going!
-        `
-        computerScore.innerHTML++;
-        rockComputer.innerHTML++;
-        scissorPlayer.innerHTML++;
-    }
-    else if(e.target.id === "scissors-icon" && computerSelection === "Scissors") {
-        PlayerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="./images/Scissor Icon.png" alt="rock paper scissors icons">
-        `
-
-        computerChoiceResult.innerHTML = `
-            <img id="player-choice-icon" src="/images/Scissor Icon right.png" alt="rock paper scissors icons">
-        `
-        roundResult.textContent = `You picked Scissors and Computer picked ${computerSelection}.
-        Twins! carry on!
-        `
-        scissorComputer.innerHTML++;
-        scissorPlayer.innerHTML++;
+    
+    
+    if(round.textContent > 4) {
+        if(playerScore.textContent > computerScore.textContent) {
+            roundResult.textContent = `Awesome, You win the game. Well done!`;
+            resetBtn.classList.remove("dont-show");
+            resetBtn.style.marginBottom = "300px";
+            choiceDiv.classList.add("dont-show");
+        }
+        else if(playerScore.textContent < computerScore.textContent) {
+            roundResult.textContent = `Computer wins this time. Better luck next time`;
+            resetBtn.classList.remove("dont-show");
+            resetBtn.style.marginBottom = "300px";
+            choiceDiv.classList.add("dont-show");
+        }
+        else if(playerScore.textContent === computerScore.textContent) {
+            roundResult.textContent = `Hmm, What do we have here, a tie`;
+            resetBtn.classList.remove("dont-show");
+            resetBtn.style.marginBottom = "300px";
+            choiceDiv.classList.add("dont-show");
+            
+        }
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+resetBtn.addEventListener("click", () => {
+    resetBtn.classList.add("dont-show");
+    rockChoiceBtn.classList.remove("dont-show");
+    choiceDiv.classList.remove("dont-show")
+    round.textContent = "";
+    roundResult.textContent = ""
+    playerChoiceResult.textContent = "";
+    computerChoiceResult.textContent = "";
+    playerScore.textContent = "";
+    computerScore.textContent = "";
+    rockPlayerScore.textContent = "";
+    paperPlayerScore.textContent = "";
+    scissorPlayerScore.textContent = "";
+    rockComputerScore.textContent = "";
+    paperComputerScore.textContent = "";
+    scissorComputerScore.textContent = "";
+})
